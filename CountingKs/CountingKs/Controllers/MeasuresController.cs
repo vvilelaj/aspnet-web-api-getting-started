ce5d814f33d1a54e170b3ff3a2e9f7e860cb1d3c
Victor Vilela <vilela_victor@hotmail.com>
2018-07-01 01:25:42 -0500

    add measure association api to food

---
+++ CountingKs/CountingKs/Controllers/MeasuresController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CountingKs.Data;
+using CountingKs.Models;
+
+namespace CountingKs.Controllers
+{
+  public class MeasuresController : ApiController
+  {
+    private readonly ICountingKsRepository _repository;
+    private readonly ModelFactory _modelFactory;
+
+    public MeasuresController(ICountingKsRepository repository)
+    {
+      _repository = repository;
+      _modelFactory = new ModelFactory();
+    }
+    public IEnumerable<MeasureModel> Get(int foodId)
+    {
+      return _repository.GetMeasuresForFood(foodId).ToList().Select(m => _modelFactory.Create(m));
+    }
+    public MeasureModel Get(int foodId, int id)
+    {
+      var measure = _repository.GetMeasure(id);
+
+      if (measure.Food.Id == foodId) return _modelFactory.Create(measure);
+
+      return null;
+    }
+  }
+}
