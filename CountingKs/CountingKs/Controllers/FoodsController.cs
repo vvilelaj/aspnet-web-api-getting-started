36f259bcbb27f670e00e208b9e673ced3d0c40d7
Victor Vilela <vilela_victor@hotmail.com>
2018-07-01 00:37:51 -0500

    add ModelFactory to return FoodModel and MeasureModel

--- CountingKs/CountingKs/Controllers/FoodsController.cs
+++ CountingKs/CountingKs/Controllers/FoodsController.cs
@@ -6,20 +6,27 @@ using System.Net.Http;
 using System.Web.Http;
 using CountingKs.Data;
 using CountingKs.Data.Entities;
+using CountingKs.Models;
 
 namespace CountingKs.Controllers
 {
   public class FoodsController : ApiController
   {
-    private ICountingKsRepository _repository;
+    private readonly ICountingKsRepository _repository;
+    private readonly ModelFactory _modelFactory;
 
     public FoodsController(ICountingKsRepository repository)
     {
       _repository = repository;
+      _modelFactory= new ModelFactory();
     }
-    public IEnumerable<Food> Get()
+    public IEnumerable<FoodModel> Get()
     {
-      var results = _repository.GetAllFoods().OrderBy(x => x.Description).Take(25).ToList();
+      var results = _repository.GetAllFoodsWithMeasures()
+        .OrderBy(x => x.Description)
+        .Take(25)
+        .ToList()
+        .Select(f => _modelFactory.Create(f));
       return results;
     }
   }
