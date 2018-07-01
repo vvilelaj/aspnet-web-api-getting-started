35cd94f88e2cd9127feea2f34396c87b6934bee4
Victor Vilela <vilela_victor@hotmail.com>
2018-06-30 19:58:39 -0500

    add FoodsController

---
+++ CountingKs/CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CountingKs.Data;
+using CountingKs.Data.Entities;
+
+namespace CountingKs.Controllers
+{
+  public class FoodsController : ApiController
+  {
+    public IEnumerable<Food> Get()
+    {
+
+      try
+      {
+        using (var context = new CountingKsContext())
+        {
+          var repo = new CountingKsRepository(context);
+
+          var results = repo.GetAllFoods().OrderBy(x => x.Description).Take(25).ToList();
+
+          return results;
+        }
+      }
+      catch (Exception ex)
+      {
+        return null;
+      }
+    }
+  }
+}
